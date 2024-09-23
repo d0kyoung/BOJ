a1465f52beedb7a218bec5c9ca5537fac52b4734
d0kyoung <ehrud0522@gmail.com>
2024-09-23 23:14:24 +0900

    problem : 2577. 숫자의 개수

--- src/Bronze2/BOJ2577.java
+++ src/Bronze2/BOJ2577.java
@@ -15,8 +15,8 @@ public class BOJ2577 {
 
         int[] count = new int[10];
 
-        for(int i=0;i<arr.length;i++){
-            int temp = Integer.parseInt(String.valueOf(arr[i]));
+        for (char value : arr) {
+            int temp = Integer.parseInt(String.valueOf(value));
             count[temp]++; // value값을 어떤 배열의 인덱스로 변환하여 의미있는 의미로 쓰인다. 부여한다.
         }
 
